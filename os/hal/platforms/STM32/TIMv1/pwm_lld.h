1c0314e3e76bcb7e227fff11d460722aed6b8ca3
TheoH <Theo0x48@gmail.com>
2015-05-02 01:16:27 -0700

    Added PWMD12 to STM32 pwm_lld

--- os/hal/platforms/STM32/TIMv1/pwm_lld.h
+++ os/hal/platforms/STM32/TIMv1/pwm_lld.h
@@ -150,6 +150,15 @@
 #endif
 
 /**
+ * @brief   PWMD9 driver enable switch.
+ * @details If set to @p TRUE the support for PWMD12 is included.
+ * @note    The default is @p TRUE.
+ */
+#if !defined(STM32_PWM_USE_TIM12) || defined(__DOXYGEN__)
+#define STM32_PWM_USE_TIM12                  FALSE
+#endif
+
+/**
  * @brief   PWMD1 interrupt priority level setting.
  */
 #if !defined(STM32_PWM_TIM1_IRQ_PRIORITY) || defined(__DOXYGEN__)
@@ -198,6 +207,14 @@
 #if !defined(STM32_PWM_TIM9_IRQ_PRIORITY) || defined(__DOXYGEN__)
 #define STM32_PWM_TIM9_IRQ_PRIORITY         7
 #endif
+
+/**
+ * @brief   PWMD12 interrupt priority level setting.
+ */
+#if !defined(STM32_PWM_TIM12_IRQ_PRIORITY) || defined(__DOXYGEN__)
+#define STM32_PWM_TIM12_IRQ_PRIORITY         7
+#endif
+
 /** @} */
 
 /*===========================================================================*/
@@ -232,10 +249,15 @@
 #error "TIM9 not present in the selected device"
 #endif
 
+#if STM32_PWM_USE_TIM12 && !STM32_HAS_TIM12
+#error "TIM12 not present in the selected device"
+#endif
+
+
 #if !STM32_PWM_USE_TIM1 && !STM32_PWM_USE_TIM2 &&                           \
     !STM32_PWM_USE_TIM3 && !STM32_PWM_USE_TIM4 &&                           \
     !STM32_PWM_USE_TIM5 && !STM32_PWM_USE_TIM8 &&                           \
-    !STM32_PWM_USE_TIM9
+    !STM32_PWM_USE_TIM9 && !STM32_PWM_USE_TIM12
 #error "PWM driver activated but no TIM peripheral assigned"
 #endif
 
@@ -277,6 +299,12 @@
     !CORTEX_IS_VALID_KERNEL_PRIORITY(STM32_PWM_TIM9_IRQ_PRIORITY)
 #error "Invalid IRQ priority assigned to TIM9"
 #endif
+
+#if STM32_PWM_USE_TIM12 &&                                                   \
+    !CORTEX_IS_VALID_KERNEL_PRIORITY(STM32_PWM_TIM12_IRQ_PRIORITY)
+#error "Invalid IRQ priority assigned to TIM12"
+#endif
+
 
 /*===========================================================================*/
 /* Driver data structures and types.                                         */
@@ -457,6 +485,10 @@ extern PWMDriver PWMD8;
 
 #if STM32_PWM_USE_TIM9 && !defined(__DOXYGEN__)
 extern PWMDriver PWMD9;
+#endif
+
+#if STM32_PWM_USE_TIM12 && !defined(__DOXYGEN__)
+extern PWMDriver PWMD12;
 #endif
 
 #ifdef __cplusplus
